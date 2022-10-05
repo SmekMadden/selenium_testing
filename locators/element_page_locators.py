cc86966fd097775dc99a284cd78065cd03dc16f3
Vlad Smek <smek1negithub@gmail.com>
2022-10-05 18:49:39 +0300

    add CheckBox tests

--- locators/element_page_locators.py
+++ locators/element_page_locators.py
@@ -14,3 +14,12 @@ class TextBoxPageLocators:
     OUTPUT_EMAIL = (By.CSS_SELECTOR, "#output #email")
     OUTPUT_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
     OUTPUT_PERM_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
+
+
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, 'button[title="Expand all"]')
+    CHECKBOX_LIST = (By.CSS_SELECTOR, 'span.rct-text')
+    CHECKED_BOXES = (By.CSS_SELECTOR, '.rct-icon-check')
+    RESULT_MESSAGE = (By.CSS_SELECTOR, '#result')
+    BOX_NAMES_FROM_RESULT = (By.CSS_SELECTOR, 'span.text-success')
+    CHECKBOX_TITLE = (By.XPATH, './/ancestor::span[@class="rct-text"]')
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,12 +1,12 @@
+import random
 import time
-
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
 from pages.base_page import BasePage
-from locators.element_page_locators import TextBoxPageLocators
+from locators.element_page_locators import TextBoxPageLocators, CheckBoxPageLocators
 
 
 class TextBoxPage(BasePage):
@@ -33,3 +33,29 @@ class TextBoxPage(BasePage):
         current_address = self.element_is_present(self.locators.OUTPUT_CURRENT_ADDRESS).text.split(':')[1]
         permanent_address = self.element_is_present(self.locators.OUTPUT_PERM_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
+
+
+class CheckBoxPage(BasePage):
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        checkbox_list = self.elements_are_visible(self.locators.CHECKBOX_LIST)
+        for _ in 'qwe':  # 3 random checkbox
+            checkbox_list[random.randrange(len(checkbox_list))].click()  # list[index].click
+
+    def compare_clicked_checkboxes_with_result_message(self):
+        checkbox_list = self.elements_are_present(self.locators.CHECKED_BOXES)
+        selected_box_names = []
+
+        for box in checkbox_list:
+            box_name = box.find_element(*self.locators.CHECKBOX_TITLE).text
+            selected_box_names.append(box_name.lower().replace('.doc', '').replace(' ', ''))
+
+        output_box_names = [name.text.lower() for name in
+                            self.elements_are_present(self.locators.BOX_NAMES_FROM_RESULT)]
+
+        assert selected_box_names == output_box_names, \
+            'Names of selected boxes and names in result message are not match'
